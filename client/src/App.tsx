cc45e9e14d294705a610f772885c795594ad9ed4
Yash Tyagi <contactyashtyagi@gmail.com>
2024-11-12 00:41:34 +0530

    added pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,27 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <header className="w-full flex bg-white justify-between items-center p-4 sm:px-8 border-b border-b-[#e6ebf4] ">
+                <Link to="/" className="font-inter font-semibold text-black text-3xl">
+                    VisualizeIt
+                </Link>
+
+                <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] px-4 py-2 rounded-md">
+                    Create
+                </Link>
+            </header>
+
+            <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                    <Route path="/create-post" element={ <CreatePost /> } />
+                </Routes>
+            </main>
+        </BrowserRouter>
     )
 }
 
